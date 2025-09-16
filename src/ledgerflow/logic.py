26a21e383212d9d35baec4773781d418a491a4bf
Logan Krause <loganckrause@outlook.com>
2025-09-16 16:34:06 -0400

    Changed the Gemini model for a higher request limit

--- src/ledgerflow/logic.py
+++ src/ledgerflow/logic.py
@@ -30,7 +30,7 @@ def auto_categorize(note, client):
     
     try:
         response = client.models.generate_content(
-            model="gemini-2.0-flash-lite", contents=prompt
+            model="gemini-2.5-flash-lite", contents=prompt
         )
         return response.text
     except Exception as e:
